ec72a535b662465a62d18a357fb607e487096eda
Phil Sung <philbert@gmail.com>
2010-06-09 23:22:52 -0700

    Update license info.

--- rdiff-snapshot-fs.py
+++ rdiff-snapshot-fs.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Usage:        ./rdiff-snapshot-fs.py <mountpoint>
+# Copyright (C) 2010 Phil Sung
+#
+# This file is part of rdiff-snapshot-fs.
+#
+# rdiff-snapshot-fs is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+# Usage:        ./rdiff-snapshot-fs.py <rdiff-backup-repository> <mountpoint>
 # Unmount with: fusermount -u <mountpoint>
 #
-# Requirements: rdiff-backup
+# Requirements: rdiff-backup, FUSE Python bindings
 
 import errno
 import fuse
